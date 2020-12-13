6d9ce48d0ebc249e022bd3b0a7cf53d01bb6b24d
jiaqd1203 <jiaqd1203@gmail.com>
2020-12-13 20:45:38 +0000

    Merge Sorted Array

---
+++ Merge Sorted Array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+/*var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m);
+    for(var i=0;i<n;i++){
+        nums1.push(nums2[i]); 
+    }
+    nums1.sort(function(a,b){
+        return a-b; 
+    })
+    return nums1;
+};*/
+
+/*var merge = function(nums1, m, nums2, n) {
+//     initialise nums1
+    nums1.splice(m);
+    nums1.push(...nums2);
+    nums1.sort((a,b) => a-b);
+};*/
