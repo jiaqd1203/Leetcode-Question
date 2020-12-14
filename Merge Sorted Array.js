f70df378b17d5a068a341f9e4f00ed026cf26dd7
jiaqd1203 <jiaqd1203@gmail.com>
2020-12-14 02:56:38 +0000

    Merge Sorted Array

--- Merge Sorted Array.js
+++ Merge Sorted Array.js
@@ -23,3 +23,17 @@
     nums1.push(...nums2);
     nums1.sort((a,b) => a-b);
 };*/
+
+var merge = function(nums1, m, nums2, n){     
+    var len1 = m - 1;
+    var len2 = n - 1;
+    var len = m + n - 1;
+    while(len1 >= 0 && len2 >= 0){  
+        //在num1和num2 中取最大值写入num1尾部
+        nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--]; 
+    }
+        //如果num2上方循环达到临界值后num2中还有数 则复制过来 num1则不需要担心比如[5,6,7,0,0,0] [1,2,3]
+        for(var i = 0; i<=len2;i++){
+            nums1[i] = nums2[i];
+        }
+  }
