b7936911c25fdcf95758f34335d35d71f1940d4c
jiaqd1203 <jiaqd1203@gmail.com>
2020-12-16 00:09:53 +0000

    Reverse Integer

--- 7.Reverse Integer.js
+++ 7.Reverse Integer.js
@@ -19,7 +19,22 @@ var reverse = function(x) {
 };
 */
 
-var reverse = function(x) {
+/*var reverse = function(x) {
     let y =  parseFloat(x.toString().split('').reverse().join(''))*Math.sign(x);
     return (y > (Math.pow(2,31)-1) || y < (Math.pow(-2,31)+1))?0:y;
-};
+};*/
+
+var reverse = function(x) {
+    let y=x;
+    x = Math.abs(x);
+    let arr = x.toString().split('');
+    let num = 0;
+    for(let i = arr.length-1; i>=0;i--){
+        num = num*10 + parseInt(arr[i]);
+    }
+    if(num >= Math.pow(2,31)-1 ||num <= -Math.pow(2,31)){
+        return 0;
+    }
+    return y>0?num:-num;
+    
+}
