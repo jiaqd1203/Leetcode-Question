d5d0c1ff8c0bb068b2112043993620efcd44a3b8
jiaqd1203 <jiaqd1203@gmail.com>
2020-12-27 18:43:20 +0000

    Sqrt(x)

--- 69.Sqrt(x).js
+++ 69.Sqrt(x).js
@@ -2,6 +2,22 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
+/*var mySqrt = function(x) {
     return Math.floor(Math.sqrt(x));
+};*/
+
+var mySqrt = function(x) {
+    var left = 0, right = x;
+    while (left < right) {
+        var mid = parseInt((left + right)/2);
+        if (mid * mid === x) {
+            return mid;
+        }
+        if (x < mid * mid) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return x < right * right ? right - 1 : right; 
 };
